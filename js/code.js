eadc940bb0c83fd6623e803148512e4efcb6412d
Michela Cerro <78146321+michelacerro@users.noreply.github.com>
2021-04-14 17:34:12 +0200

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,1 +1,13 @@
+let value = document.getElementById('counter-value');
 
+let increase = function() {value.innerHTML++;}
+let plus = document.getElementById('plus');
+plus.addEventListener('click', increase);
+
+let reduce = function() {value.innerHTML--;}
+let minus = document.getElementById('minus');
+minus.addEventListener('click', reduce);
+
+let restart = function() {value.innerHTML = 0;}
+let reset = document.getElementById('reset');
+reset.addEventListener('click', restart);
